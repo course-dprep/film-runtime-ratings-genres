d2e71d5b63e8fb9f2d26a3857488e6db52860475
Jelle van der Eng <j.b.vdreng@tilburguniversity.edu>
2025-09-28 22:45:07 +0200

    Update Data filtering.R
    Updated the code for data filtering to be more efficient, and have comments explaining every step

--- src/data-preparation/Data filtering.R
+++ src/data-preparation/Data filtering.R
@@ -1,8 +1,10 @@
+#load packages
 library(dplyr)
 library(tidyverse)
 library(readr)
 
-#merging datasets
+#opening datasets
+#uses data dowloaded in download-data.R
 
 #basics <- read_tsv("title.basics.tsv")
 #ratings <- read_tsv("title.ratings.tsv")
@@ -18,47 +20,32 @@ library(readr)
 #summary(ratings)
 
 
-#cbind(basics, ratings)
+#Merging datasets
+merged_unfiltered <- left_join(ratings, basics, by = "tconst")
 
-#projectdf <- left_join(ratings, basics, by = "tconst")
+#Identifying NA's
+merged_unfiltered <- merged_unfiltered %>%
+  mutate(across(everything(), ~ ifelse(.x == "\\N", NA, .x)))
 
-#remove NA and write df
-#merged_projectdf <- read_csv("IMDBdatasetTA.csv", na = "\\N") 
-#write.csv(merged_projectdf, "Merged_IMDB_unfiltered.csv", row.names = FALSE)
-
-
-merged_unfiltered <- read_csv("Merged_IMDB_unfiltered.csv")
-
-
-#filter dataset using intermediate dataframe 'filtered_df'
-
+#filter to only keep the movies
 filtered_df <- merged_unfiltered %>%
  filter(titleType == "movie")
 
+#filter for movies that have 250 or more reviews (numVotes) 
 filtered_df <- filtered_df %>%
   filter(numVotes >= 250)
 
-str(filtered_df$runtimeMinutes)
-filtered_df$runtimeMinutes <- as.numeric(filtered_df$runtimeMinutes)
-
-
-# this keeps the year it was published, could remove later
+# filter only for the relevant columns
 filtered_df <- filtered_df %>%
-  select(-originalTitle, -isAdult, -endYear, -titleType)
+  select(tconst, averageRating, runtimeMinutes, genres)
 
-# make clean dataset
+# changes runtime in minutes from a character variable to a numeric variable
+str(filtered_df$runtimeMinutes)
+  filtered_df$runtimeMinutes <- as.numeric(filtered_df$runtimeMinutes)
 
+# filters for movies with a runtime between 30 and 250 minutes to remove outliers
 movies_clean <- subset(filtered_df, !is.na(runtimeMinutes) & runtimeMinutes > 30 & runtimeMinutes < 250 & !is.na(genres))
 
 
 write_csv(movies_clean, "movies_clean.csv")
-
-
-
-# unused attempts to make plots
-
-#movies_clean %>% ggplot(aes(x = runtimeMinutes)) +
- #                    geom_histogram(binwidth = 5)
-#max(filtered$runtimeMinutes)
-
 
