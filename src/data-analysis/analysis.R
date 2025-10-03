493bc820b86e53a9aeb98a0af988d1f3e0a3918c
roosvandenberg <r.vdnberg@tilburguniversity.edu>
2025-10-03 20:52:59 +0200

    Update analysis.R
    I add the analysis script with the modeling. Same for this, I have some RStudio issues right now so also not sure if this works but I'll try it again later

--- src/data-analysis/analysis.R
+++ src/data-analysis/analysis.R
@@ -1,1 +1,57 @@
-# In this directory, you will keep all source code related to your analysis.
+# DATA ANALYSIS
+
+#Loading libraries
+library(tidyverse)
+library(dplyr)
+library(readr)
+library(here) 
+library(MASS)
+library(car)
+
+# Define analysis output directory
+analysis_output_dir <- here("gen", "data_analysis", "output")
+
+# Create analysis output directory if it doesn't exist
+if(!dir.exists(analysis_output_dir)) {
+  dir.create(analysis_output_dir, recursive = TRUE)
+  message(paste("Analysis output directory created at:", analysis_output_dir))
+} else {
+  message(paste("Analysis output directory already exists at:", analysis_output_dir))
+}
+
+# START WITH EXPLORATORY ANALYSIS so without moderator
+# Convert rating_category to numeric for exploratory analyses, since these analyses require numeric DV's
+cleaned_movies <- cleaned_movies %>% 
+  mutate(rating_num = as.numeric(rating_category))
+
+# exploratory linear regression: simple linear model
+linear_regression <- lm((rating_num ~ runtime_min), cleaned_movies)
+sink(file.path(analysis_output_dir, "linear_regression_summary.txt"))
+summary(linear_regression)
+sink()
+
+# exploratory non-linear model: quadratic model
+quadratic_model <- lm(rating_num ~ runtime_min + I(runtime_min^2), cleaned_movies)
+sink(file.path(analysis_output_dir, "quadratic_model_summary.txt"))
+summary(quadratic_model)
+sink()
+
+# exploratory non-linear model: logarithmic model
+logarithmic_model <- lm(rating_num ~ log(runtime_min), cleaned_movies)
+sink(file.path(analysis_output_dir, "logarithmic_model_summary.txt"))
+summary(logarithmic_model)
+sink()
+
+# ordinal logistic regression seems the best though, because it doesn't assume equal spacing between categories
+ordinal_log_regression <- polr(rating_category ~ runtime_min, data = cleaned_movies, Hess = TRUE)
+sink(file.path(analysis_output_dir, "ordinal_log_regression_summary.txt"))
+summary(ordinal_log_regression)
+Anova(ordinal_log_regression)
+sink()
+
+# ordinal logistic regression with moderator
+ordinal_log_regression2 <- polr(rating_category ~ runtime_min * genre_list, data = cleaned_movies, Hess = TRUE)
+sink(file.path(analysis_output_dir, "ordinal_log_regression_with_genre_summary.txt"))
+summary(ordinal_log_regression2)
+Anova(ordinal_log_regression2)
+sink()
